855fc1558b32df9780d553c84adfd7f55b485464
Vladyslav Zavedenski <zavedenski@gmail.com>
2017-12-25 00:50:43 +0100

    Make some changes, and loop works well.

--- likeQuiz.py
+++ likeQuiz.py
@@ -10,13 +10,13 @@ name = str(input("What's your name: "))
 sex = str(input("Who are you? Boy|Girl: "))
 technologies = str(input("What's your favorite technology: "))
 
-for a in range(myName):
+for a in range(len(myName)):
     if name == myName[a]:
         print("Cool, we have a same names!")
         pass
     pass
-for a in range(myTechnologies):
+for a in range(len(myTechnologies)):
     if technologies == myTechnologies[a]:
-        print("Hmm, {tech} good choise ".format(tech == myTechnologies[a]))
+        print("Hmm, {tech} good choise ".format(tech = myTechnologies[a]))
         pass
     pass
