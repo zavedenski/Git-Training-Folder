e0ab23243cbf33b64b637170e3e8620960a29b3f
Vladyslav Zavedenski <zavedenski@gmail.com>
2017-12-24 22:24:32 +0100

    I was create somethink like quiz.

--- likeQuiz.py
+++ likeQuiz.py
@@ -1,1 +1,22 @@
-print "First file which, I upload into GitHub!"
+print ("Hello, it's my third commit with git!")
+
+myName = ['Vladyslav', 'Vlad']
+mySex = 'Male'
+myTechnologies = ['Python', 'C++', 'SQL']
+
+
+print("So, maybe you can tell something about yourself =) ")
+name = str(input("What's your name: "))
+sex = str(input("Who are you? Boy|Girl: "))
+technologies = str(input("What's your favorite technology: "))
+
+for a in range(myName):
+    if name == myName[a]:
+        print("Cool, we have a same names!")
+        pass
+    pass
+for a in range(myTechnologies):
+    if technologies == myTechnologies[a]:
+        print("I like {tech} too".format(tech == myTechnologies[a]))
+        pass
+    pass
