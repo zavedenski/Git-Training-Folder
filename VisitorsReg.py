76442a4916c8ccf4f214062626352327fc7b6d10
Vladyslav Zavedenski <zavedenski@gmail.com>
2017-12-25 13:25:03 +0100

    I was create new file with classes and methods, and import it to 'VisitorsReg.py', it's make code more clear and usefull.

--- VisitorsReg.py
+++ VisitorsReg.py
@@ -1,39 +1,17 @@
+from additionalFile import *
 
-fullName = 'Vladyslav Zavedenski'
-Age = 20
-Sex = 'Male'
-myTechnologies = ['Python', 'C++', 'STL', 'Qt', 'SQL', 'Html', 'CSS', 'Django', 'Flask']
-#опыт
+person = Worker()
 
 print("Welcome to Visitors Registration form")
 print("If you are looking for workers, please fill this field.")
 
-date = str(input("Please enter date (dd/mm/yy): "))
-company = str(input("Name of company: "))
-email = str(input("Email for contact: "))
-experience = float(input("Minimal professional experience (1.5 years format): "))
-degree = str(input("Minimal degree: "))
-mathes = 0
-skills = []
-while True:
-    print("If you want to finish, Enter 'quit'. ")
-    skill = str(input("Enter necessary skill: "))
-    if skill != 'quit':
-        skills.append(skill)
-    else:
-        break
+employer = Recruiter()
+employer.putData()
 
-for i in range(len(skills)):
-    for j in range(len(myTechnologies)):
-        if skills[i] == myTechnologies[j]:
+mathes = 0
+for i in range(len(employer.skills)):
+    for j in range(len(person.skills)):
+        if employer.skills[i] == person.skills[j]:
             mathes += 1
 
-
-openFile = open("RegFile.txt", "a")
-openFile.write("Date of Registration form: {d} \n".format(d = date))
-openFile.write("Name of Company: {c} \n".format(c = company))
-openFile.write("Email for contant: {e} \n".format(e = email))
-openFile.write("Minimal professional experience: {ex} \n".format(ex = experience))
-openFile.write("Minimal degree: {deg} \n".format(deg = degree))
-openFile.write("Count of required skills: {m} \n".format(m = mathes))
-openFile.close()
+putDataIntoFile(employer.nameOfCompany, employer.email, employer.experience, employer.degree, mathes)
---
+++ additionalFile.py
@@ -0,0 +1,64 @@
+from time import gmtime, strftime
+
+class Worker:
+    def __init__(self):
+        self.fullName = "Vladyslav Zavedenski"
+        self.nationality = "Poland"
+        self.sex = "Male"
+        self.age = 20
+        self.degree = "Bachelor of computer science"
+        self.experience = 1.5
+        self.skills = ['Python', 'Django', 'Flask', 'C++', 'STL', 'Qt', 'SQL', 'Html', 'CSS']
+        pass
+
+    def info(self):
+        print("Full Name    ==> ", self.fullName)
+        print("Nationality  ==> ", self.nationality)
+        print("Sex          ==> ", self.sex)
+        print("Age          ==> ", self.age)
+        print("Degree       ==> ", self.degree)
+        print("Experience   ==> ", self.experience, "years")
+        print("Technologies ==> ", self.skills)
+        pass
+    pass
+
+class Recruiter:
+    def __init__(self):
+        self.nameOfCompany  = "None"
+        self.email          = "None"
+        self.experience     = 0
+        self.degree         = "None"
+        self.skills         = []
+        self.message        = "None"
+        pass
+
+    def putData(self):
+        self.nameOfCompany  = str(input("Name of company: "))
+        self.email          = str(input("Email for contact: "))
+        self.experience     = float(input("Minimal professional experience (1.5 years format): "))
+        self.degree         = str(input("Minimal degree: "))
+
+        print("If you want to finish, Enter 'quit'. ")
+        while True:
+            skill = str(input("Enter required skill: "))
+            if skill != 'quit':
+                self.skills.append(skill)
+            else:
+                break
+            pass
+
+        self.message        = str(input("Enter some message (Not required): "))
+        pass
+    pass
+
+
+def putDataIntoFile(company, email, experience, degree, matchedSkills):
+    readFile = open("RegFile.txt", "a")
+    readFile.write("Date of Registration form: {d} \n".format(d = strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+    readFile.write("Name of Company: {c} \n".format(c = company))
+    readFile.write("Email for contant: {e} \n".format(e = email))
+    readFile.write("Minimal professional experience: {ex} \n".format(ex = experience))
+    readFile.write("Minimal degree: {deg} \n".format(deg = degree))
+    readFile.write("Count of required skills: {m} \n".format(m = matchedSkills))
+    readFile.close()
+    pass
